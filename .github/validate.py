7339b039a5a871c468aa2d0d1c36ed4b224d19b1
Ajeet Yadav <ajeetyadav164@gmail.com>
2022-11-08 02:52:32 +0530

    bug fix

--- .github/validate.py
+++ .github/validate.py
@@ -37,23 +37,26 @@ schemaName = ""
 schemaType = ""
 for i in tableName:
     if "Schema" in i or "schema" in i or "SCHEMA" in i:
+        print(i)
         schemaName = i
     if "table" in i or "Table" in i or "TABLE" in i:
         schemaType = "Tables"
     elif "view" in i or "View" in i or "VIEW" in i:
         schemaType = "Views"
-    elif "PROCEDURE" in i or "Proedure" in i or "proedure" in i:
-        schemaType = "Stored Procedures"
+    elif "PROCEDURE" in i or "Procedure" in i or "procedure" in i:
+        schemaType = "Procedures"
     else:
         continue
+
 if schemaName == "" or schemaType == "":
-    print(" please place the files in correct location ")
+    print("place the files in correct location ")
     exit(1)
 splitSchemaName = schemaName.split('.')
 checkPath = splitSchemaName[0]+"/"+schemaType
+
 if checkPath in path and checkPath != "/":
-    print(" files locations are correct ")
+    print("files locations are correct ")
     exit(0)
 else:
-    print(" please place the files in correct location ")
+    print("place the files in correct location ")
     exit(1)
